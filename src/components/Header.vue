902ff5c3bfae64e5170d39fa63fc02b14f85c720
Thomas Bricker <thhomasbricker87@gmail.com>
2020-08-16 14:00:31 +0300

    Corected button in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-container ><!-- ??? -->
+  <v-container>
     <v-row>
-      <!-- <HeaderNavMenu /> -->
       <v-col cols="12" order="2" xs="12" order-md="1" md="6">
         <v-row>
           <v-card flat class="mt-15 header-text">
@@ -9,13 +8,18 @@
             <p
               class="mt-5"
             >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quaerat est sit? Labore accusamus doloremque eum porro? Quo iste placeat nihil alias amet!</p>
-            <v-btn class="mt-15 buttons header-btn">Lorem ipsum</v-btn>
+            <v-btn
+              width="280"
+              height="70"
+              color="#109CF1"
+              class="mt-15 buttons header-btn"
+            >Lorem ipsum</v-btn>
           </v-card>
         </v-row>
       </v-col>
-      <v-col cols="12" order="1" xs="12" order-md="2" md="6" >
+      <v-col cols="12" order="1" xs="12" order-md="2" md="6">
         <v-row>
-          <v-img src="../assets/header.svg" ></v-img>
+          <v-img src="../assets/header.svg"></v-img>
         </v-row>
       </v-col>
     </v-row>
@@ -33,16 +37,19 @@
   }
   .header-btn {
     margin: 0 auto;
+    box-shadow: 0px 4px 10px rgba(16, 156, 241, 0.24);
+    border-radius: 4px;
+    text-transform: none;
   }
 }
 </style>
+
 <script>
-// import HeaderNavMenu from '@/components/HeaderNavMenu.vue'
 
 export default {
   name: 'Header',
   components: {
-    // HeaderNavMenu,
+    //
   },
 }
 </script>
