e02e0aa41f96a3436ee0a153abfe356c7c975fb4
Thomas Bricker <thhomasbricker87@gmail.com>
2020-08-16 17:57:31 +0300

    Changed style section 'AboutUs'

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container fluid>
-    <h1 class="mt-15">About us</h1>
+    <h2 class="mt-15 section-title">About us</h2>
     <v-row>
       <v-col cols="12" order="2" xs="12" order-md="1" md="6">
           <v-card flat class="mt-15 about-text">
-            <h1 class="mt-15">Lorem ipsum dolor sit amet</h1>
+            <h1 class="mt-15 title">Lorem ipsum dolor sit amet</h1>
             <p
-              class="mt-5"
+              class="mt-5 description"
             >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </v-card>
       </v-col>
@@ -19,17 +19,40 @@
 </template>
 
 <style scoped>
-@media screen and (max-width: 960px) {
-  .about-text h1 {
-    text-align: center;
-    margin-top: 0 !important;
-  }
-  .about-text {
-    margin-top: 0 !important;
-  }
-  .about-btn {
-    margin: 0 auto;
-  }
+.section-title {
+  margin-bottom: 35px;
+
+font-family: Oswald;
+font-style: normal;
+font-weight: bold;
+font-size: 46px;
+line-height: 68px;
+
+color: #000000;
+}
+.title {
+  margin-top: 100px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 150%;
+
+color: #000000;
+}
+.description {
+  margin-top: 23px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 16px;
+line-height: 150%;
+/* or 24px */
+
+
+color: #000000;
 }
 </style>
 
