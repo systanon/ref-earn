46f3dbe11dc943786a83d75922edef49c59d9f34
Sergei (via Travis CI) <tustanovskiis@gmail.com>
2020-08-18 13:15:18 +0000

    Deploy vue-command/refEarn to github.com/vue-command/refEarn.git:gh-pages

---
+++ js/chunk-2d22d746.2bd4ada6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,e){"use strict";e.r(t);var u=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("AboutUs")},o=[],s=e("2089"),c={name:"About",components:{AboutUs:s["a"]}},a=c,l=e("2877"),r=Object(l["a"])(a,u,o,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.2bd4ada6.js.map
