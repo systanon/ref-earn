06c3ecef2b5a4c8f18641e3fb5d39f2952587bf5
Sergei <tustanovskiis@gmail.com>
2020-08-18 16:34:28 +0300

    change ref-earn

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/refEarn/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/ref-earn/' : '/',
 }
